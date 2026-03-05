c7b08441d9d1a9195ad00b5c65ec30c6bf45cf6e
IgnasAns <ignas@example.com>
2026-03-05 19:24:11 +0200

    chore: update build_release.bat to build both APK and AAB

--- build_release.bat
+++ build_release.bat
@@ -2,4 +2,8 @@
 set JAVA_HOME=C:\Program Files\Microsoft\jdk-17.0.17.10-hotspot
 set PATH=%JAVA_HOME%\bin;%PATH%
 cd /d "C:\Applications\Habit tracker\HabitFlow"
+echo Building APK (assembleRelease)...
 call android\gradlew.bat -p android assembleRelease
+echo Building AAB (bundleRelease)...
+call android\gradlew.bat -p android bundleRelease
+echo Done! APK and AAB built.
